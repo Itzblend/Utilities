7439efc098ed333110f271c2505cbc0a6dcb5c82
Lauri Huhta <laurihuhta@Lauri-MacBook-Pro.local>
2020-10-11 19:56:04 +0300

    sync

---
+++ datastuff/sys_performance/sql/database_size_v.sql
@@ -0,0 +1,13 @@
+CREATE VIEW database_sizes AS(
+    SELECT d.datname as Name,  pg_catalog.pg_get_userbyid(d.datdba) as Owner,
+    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+        THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
+        ELSE 'No Access'
+    END as Size
+FROM pg_catalog.pg_database d
+    order by
+    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+        THEN pg_catalog.pg_database_size(d.datname)
+        ELSE NULL
+    END desc -- nulls first
+);
--- datastuff/sys_performance/sql/sys_info_t.sql
+++ datastuff/sys_performance/sql/sys_info_t.sql
@@ -6,4 +6,10 @@ CREATE TABLE IF NOT EXISTS system_data_t.system_info_t (
     disk_free NUMERIC,
     disk_percent NUMERIC,
     timestamp TIMESTAMP DEFAULT (current_timestamp AT TIME ZONE 'GMT-3')
-)
+);
+
+CREATE INDEX timestamp_idx ON system_data_t.system_info_t (timestamp);
+
+CREATE INDEX cpu_percent_idx ON system_data_t.system_info_t (cpu_percent);
+CREATE INDEX disk_percent_idx ON system_data_t.system_info_t (disk_percent);
+CREATE INDEX ram_percent_idx ON system_data_t.system_info_t (ram_percent);
