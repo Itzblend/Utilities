77ab1396ad401eaf2724b1f6564357f54a8e68db
Itzblend <huhta.lauri@gmail.com>
2020-10-12 10:23:42 +0300

    table is back

---
+++ datastuff/sys_performance/sql/sys_info_t.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS system_data_t.system_info_t (
+    cpu_percent NUMERIC,
+    cpu_freq NUMERIC,
+    disk_total_gb NUMERIC,
+    disk_used_gb NUMERIC,
+    disk_free_gb NUMERIC,
+    disk_percent NUMERIC,
+    ram_used_gb NUMERIC,
+    ram_free_gb NUMERIC,
+    ram_percent NUMERIC,
+    timestamp TIMESTAMP DEFAULT (current_timestamp AT TIME ZONE 'GMT-3')
+);
+
+CREATE INDEX timestamp_idx ON system_data_t.system_info_t (timestamp);
+
+CREATE INDEX cpu_percent_idx ON system_data_t.system_info_t (cpu_percent);
+CREATE INDEX disk_percent_idx ON system_data_t.system_info_t (disk_percent);
+CREATE INDEX ram_percent_idx ON system_data_t.system_info_t (ram_percent);
