094eef6fc56112f8e79251acb1e8de06f760c168
Itzblend <huhta.lauri@gmail.com>
2021-03-28 15:34:42 +0300

    Update applymap.py

--- datastuff/applymap.py
+++ datastuff/applymap.py
@@ -1,7 +1,14 @@
 # All props to @salt rock lamp at Data Science Discord community
+
 data['name_tokens']= data['name'].map(lambda y: thread_first(
     y,
     clean_punctuation,
     split_words,
     filter_stopwords,
 ))
+
+
+data['name_tokens']= data['name']\
+    .map(clean_punctuation)\
+    .map(split_words)\
+    .map(filter_stopwords)
